03620b6e5c215a30a2e3e0b7c27351bbbb14fbc8
jobizzness <jobizzness@gmail.com>
2018-08-11 22:46:05 +0800

    orders template and styles

--- frontend/src/pages/remi-orders/index.js
+++ frontend/src/pages/remi-orders/index.js
@@ -16,9 +16,8 @@ import '@vaadin/vaadin-grid/vaadin-grid-selection-column.js';
 import '../../components/remi-order-details.js';
 import { store } from '../../store.js';
 import template from './template.html';
-import { InjectGlobalStyle } from '../../core/utils.js';
+import './style.css';
 
-InjectGlobalStyle({ name: 'remi-orders' }, () => import('./style.html'));
 /**
  * `bn-project` Description
  *
